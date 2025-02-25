8188f754a739b66da36275ad2077253446d19ef2
belal <rhsan1565@gmail.com>
2025-02-25 12:56:34 +0000

    server

--- server/core/urls.py
+++ server/core/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from .views import update_x_Y
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('update/<str:data>' , update_x_Y)
 ]
